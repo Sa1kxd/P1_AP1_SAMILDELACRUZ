ef84ba3448f18c9fcb2c24039179f41a6f0ffb6d
Sa1kxd <samildelacruz1@gmail.com>
2025-09-22 20:24:50 -0400

    Creacion del modelo

---
+++ P1_AP1_SAMILDELACRUZ/Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿namespace P1_AP1_SAMILDELACRUZ.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+
+    public string Nombre { get; set; }
+}
+
