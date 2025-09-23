829a8f8e5c38e8c19cb5fb871d75bf76152ed29d
Sa1kxd <samildelacruz1@gmail.com>
2025-09-22 20:40:07 -0400

    Creacion del services

---
+++ P1_AP1_SAMILDELACRUZ/Services/RegistroServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_SAMILDELACRUZ.DAL;
+using P1_AP1_SAMILDELACRUZ.Models;
+using System.Linq.Expressions;
+
+namespace P1_AP1_SAMILDELACRUZ.Services;
+
+public class RegistroServices(IDbContextFactory<Contexto> factory)
+{
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio) 
+    {
+        using var context = await factory.CreateDbContextAsync();
+        return await context.Registros.
+            Where(criterio).
+            AsNoTracking().
+            ToListAsync();
+    }
+}
