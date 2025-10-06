7991e846573bbc1475e233d6ccfc3a97a25a9e19
Sa1kxd <samildelacruz1@gmail.com>
2025-10-06 13:53:39 -0400

    Añadiendo validaciones al modelo

--- P1_AP1_SAMILDELACRUZ/Models/EntradasHuacales.cs
+++ P1_AP1_SAMILDELACRUZ/Models/EntradasHuacales.cs
@@ -7,6 +7,7 @@ public class EntradasHuacales
     public int IdEntrada { get; set; }
     
     [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0.")]
     public int Cantidad { get; set; }
 
     [Required(ErrorMessage = "Fecha es requerida")]
@@ -17,6 +18,7 @@ public class EntradasHuacales
     public String NombreCliente { get; set; }
 
     [Required(ErrorMessage ="Precion requerido")]
+    [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0.")]
     public double Precio { get; set; }
 
 }
