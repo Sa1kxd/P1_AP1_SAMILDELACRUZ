da1eb46b62bbf7ee889a7bd45e447caa465c3526
Sa1kxd <samildelacruz1@gmail.com>
2025-10-10 21:42:04 -0400

    Actualizacion del contexto

--- P1_AP1_SAMILDELACRUZ/DAL/Contexto.cs
+++ P1_AP1_SAMILDELACRUZ/DAL/Contexto.cs
@@ -7,4 +7,29 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     public DbSet<EntradasHuacales> Huacales { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+    public DbSet<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TiposHuacales>().HasData(
+            new List<TiposHuacales>
+            {
+                new()
+                {
+                    TipoId = 1,
+                    Descripcion = "Verde",
+                    Existencia = 0
+                },
+                new()
+                {
+                    TipoId = 2,
+                    Descripcion = "Rojo",
+                    Existencia = 0
+                }
+
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
 }
