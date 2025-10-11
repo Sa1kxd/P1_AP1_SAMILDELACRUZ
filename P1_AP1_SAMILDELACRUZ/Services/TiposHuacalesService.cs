4de1cb0f04268cf5ea4e01993db9f13fd950d5bb
Sa1kxd <samildelacruz1@gmail.com>
2025-10-10 21:45:04 -0400

    Creacion de TiposHuacalesService

---
+++ P1_AP1_SAMILDELACRUZ/Services/TiposHuacalesService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_SAMILDELACRUZ.DAL;
+using P1_AP1_SAMILDELACRUZ.Models;
+using System.Linq.Expressions;
+
+namespace P1_AP1_SAMILDELACRUZ.Services;
+
+public class TiposHuacalesService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<TiposHuacales> Buscar(int tipoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.TiposHuacales
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<TiposHuacales>> Listar(Expression<Func<TiposHuacales, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.TiposHuacales
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
