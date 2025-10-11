f5672ee9fdd256452fba7b8f11653ba16aa6055f
Sa1kxd <samildelacruz1@gmail.com>
2025-10-10 21:18:28 -0400

    Creacion de Ti[posHuacales

---
+++ P1_AP1_SAMILDELACRUZ/Models/TiposHuacales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_SAMILDELACRUZ.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "La descripcion es requerida")]
+    public string Descripcion { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "La existencia no puede ser inferior a 0")]
+    public int Existencia { get; set; }
+
+    [InverseProperty("TipoHuacal")]
+    public virtual ICollection<EntradasHuacalesDetalle> EntradaHuacalDetalle { get; set; } = new List<EntradasHuacalesDetalle>();
+}
