23a3dfe5ef4486ddea8f60df8bd2545ee8d3609c
Sa1kxd <samildelacruz1@gmail.com>
2025-10-10 21:18:48 -0400

    Creacion de EntradasHuacalesDetalle

---
+++ P1_AP1_SAMILDELACRUZ/Models/EntradasHuacalesDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_SAMILDELACRUZ.Models;
+
+public class EntradasHuacalesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int IdEntrada { get; set; }
+
+    public int TipoId { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public double Precio { get; set; }
+
+    [ForeignKey("IdEntrada")]
+    [InverseProperty("EntradaHuacalDetalle")]
+    public virtual EntradasHuacales EntradaHuacal { get; set; }
+
+
+
+    [ForeignKey("TipoId")]
+    [InverseProperty("EntradaHuacalDetalle")]
+    public virtual TiposHuacales TipoHuacal { get; set; }
+}
